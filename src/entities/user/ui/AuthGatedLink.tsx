3c3abcf47138e539ed3358e850451318d4639f32
Banana-boss <rlatjrwn1121@gmail.com>
2026-07-24 17:30:17 +0900

    [feat/#67] 비로그인 시 로그인 유도 UX 개선
    App.tsx의 누락된 PreDiagnosisPage import도 함께 수정(빌드 깨짐)

--- src/entities/user/ui/AuthGatedLink.tsx
+++ src/entities/user/ui/AuthGatedLink.tsx
@@ -1,11 +1,37 @@
 import { Link, type LinkProps } from 'react-router-dom';
 
 import { useAuthStore } from '../model/authStore';
+import { useLoginPromptStore } from '../model/loginPromptStore';
 
-function AuthGatedLink({ to, ...props }: LinkProps) {
+function AuthGatedLink({ to, className, children, ...props }: LinkProps) {
   const isAuthenticated = useAuthStore((state) => Boolean(state.user));
+  const showLoginPrompt = useLoginPromptStore((state) => state.show);
 
-  return <Link to={isAuthenticated ? to : '/login'} {...props} />;
+  if (isAuthenticated) {
+    return (
+      <Link to={to} className={className} {...props}>
+        {children}
+      </Link>
+    );
+  }
+
+  return (
+    <span
+      role="button"
+      tabIndex={0}
+      className={className}
+      style={{ cursor: 'pointer' }}
+      onClick={showLoginPrompt}
+      onKeyDown={(event) => {
+        if (event.key === 'Enter' || event.key === ' ') {
+          event.preventDefault();
+          showLoginPrompt();
+        }
+      }}
+    >
+      {children}
+    </span>
+  );
 }
 
 export default AuthGatedLink;
