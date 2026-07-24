00d1a213e8e1084c443eea07ec855b609c289cb7
CMINO99 <qnmuiop@yonsei.ac.kr>
2026-07-24 14:48:25 +0900

    Revert "[Feat/#44] 감사 페이지(고영향 AI 사전진단 + 모델 감사 실행) 구현 "

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,7 +6,6 @@ import {
 } from 'react-router-dom';
 
 import AuthInitializer from '../features/auth/model/AuthInitializer';
-import AuditPage from '../pages/audit/ui/AuditPage';
 import FindPasswordPage from '../pages/find-password/ui/FindPasswordPage';
 import FeaturesPage from '../pages/features/ui/FeaturesPage';
 import HomePage from '../pages/home/ui/HomePage';
@@ -55,12 +54,6 @@ function App() {
             <Route
               path="/signup"
               element={<SignupPage />}
-            />
-
-            {/* 감사 */}
-            <Route
-              path="/audit"
-              element={<AuditPage />}
             />
 
             <Route
